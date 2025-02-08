63cca489a7ab16fb9a168ec043161c79f890f5d6
Mariya Aliseychic <lady.urban2010@yandex.ru>
2025-02-09 02:22:26 +0500

    сдача работы

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardPlace = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard(contentCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardDeliteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').src = contentCard.link;
+    cardElement.querySelector('.card__image').alt = contentCard.name;
+    cardElement.querySelector('.card__title').textContent = contentCard.name;
+    cardDeliteButton.addEventListener('click', deliteCard);
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+    function deliteCard(el) {
+        const cardElement = el.target.closest('.card');
+        cardElement.remove();
+    }
 // @todo: Вывести карточки на страницу
+function displayCard(initialCards) {
+    initialCards.forEach(cards => {
+        const card = createCard(cards);
+        cardPlace.append(card);
+    });
+}
+displayCard(initialCards);
